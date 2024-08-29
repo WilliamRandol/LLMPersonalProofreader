65e7d0c9aafd7e0c787a125e339ac34b554732d1
William Randol <william.randol@gmail.com>
2024-08-29 11:24:23 -0600

    update main model

--- src/js/script.js
+++ src/js/script.js
@@ -195,7 +195,7 @@ window.addEventListener('load', async () => {
         const models = await modelsResponse.json();
         toggleWaiting();
 
-        message.model = models.data.some(m => m.id === 'gpt-4-1106-preview') ? 'gpt-4-1106-preview' : 'gpt-3.5-turbo-1106';
+        message.model = models.data.some(m => m.id === 'chatgpt-4o-latest') ? 'chatgpt-4o-latest' : models.data.at(-1).id;
         document.querySelector('footer').innerHTML = `Using Model: ${message.model}.`;
     }
 
